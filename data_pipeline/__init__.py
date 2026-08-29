a4917c0a814db5bcd0112dbd77395ddd2f76186a
Chandanraj M <ugcet2401335@reva.edu.in>
2026-08-30 02:09:22 +0530

    feat: Add OpsNexus API client for real telemetry data integration

--- data_pipeline/__init__.py
+++ data_pipeline/__init__.py
@@ -1,3 +1,10 @@
 """
 OpsNexus-ML Data Pipeline Package
 """
+
+from .opsnexus_client import OpsNexusClient, create_opsnexus_client
+
+__all__ = [
+    'OpsNexusClient',
+    'create_opsnexus_client'
+]
